705bee5dd59cd5829a705aa8273751e4dce7c416
Md Hasanuzzaman <dr.has82@gmail.com>
2024-05-30 18:10:01 +0600

    [ADDED]Contact form created

--- pages/Contact Me.py
+++ pages/Contact Me.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
+
 st.header("Contact Me")
+
+
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Write your message")
+    button = st.form_submit_button("Send Message")
+    if button:
+        print("I was pressed")
