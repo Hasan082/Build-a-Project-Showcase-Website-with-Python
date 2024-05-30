730fc8575224dad4457d5beb6163c0ff9219620f
Md Hasanuzzaman <dr.has82@gmail.com>
2024-05-31 02:24:55 +0600

    [ADDED] Send Email function added with button

--- pages/Contact Me.py
+++ pages/Contact Me.py
@@ -1,13 +1,24 @@
 import streamlit as st
-
+from send_email import send_email
 
 st.header("Contact Me")
 
-
-
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
-    message = st.text_area("Write your message")
+    raw_message = st.text_area("Write your message")
+    
+    message = f"""\
+Subject: Email from the website by {user_email}
+
+From: {user_email}
+{raw_message}
+    """
+
     button = st.form_submit_button("Send Message")
+       
     if button:
-        print("I was pressed")
+        if user_email and raw_message:  # Check that both fields are filled
+            send_email(message)
+            st.info("We received your email! I will contact you as soon as possible.")
+        else:
+            st.error("Please provide both an email address and a message.")
--- send_email.py
+++ send_email.py
@@ -1,21 +1,12 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
-
-username = "dr.has82@gmail.com"
-password = "oiwckoqcgghsjsuo"
-
-context = ssl.create_default_context()
-
-receiver = "drhasan201@gmail.com"
-message = """\
-Subject: Hi!
-
-How are you?
-Bye
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "dr.has82@gmail.com"
+    password = "oiwckoqcgghsjsuo"
+    receiver = "dr.has82@gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
